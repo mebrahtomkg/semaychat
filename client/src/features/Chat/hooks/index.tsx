e50ee42094f333848db86bb31b64cac722cc0696
Mebrahtom Kidane <mebrahtomkg1@gmail.com>
2025-08-27 14:29:55 +0300

    refactor: adapt ReactQuery in fetching a user, remove old redux based messages selector hook.

--- client/src/features/Chat/hooks/index.tsx
+++ client/src/features/Chat/hooks/index.tsx
@@ -4,7 +4,6 @@ export { default as useFilesSelector } from './useFilesSelector';
 export { default as useMessageActions } from './useMessageActions';
 export { default as useMessageContent } from './useMessageContent';
 export { default as useMessageInfo } from './useMessageInfo';
-export { default as useMessagesSelector } from './useMessagesSelector';
 export { default as useMessageStatus } from './useMessageStatus';
 export { default as usePendingMessages } from './usePendingMessages';
 export { default as useChatMessages } from './useChatMessages';
--- client/src/hooks/useUserFetcher.ts
+++ client/src/hooks/useUserFetcher.ts
@@ -1,33 +1,20 @@
-import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '.';
-import { userAdded } from '../usersSlice';
 import { User } from '../types';
 import { useQuery } from '@tanstack/react-query';
 import { ApiError, get } from '@/api';
 
 const useUserFetcher = (userId: number) => {
-  const users = useAppSelector((state) => state.users);
-
-  const user = users.find((user) => user.id === userId);
-
-  const dispatch = useAppDispatch();
-
   const { isError, data, error } = useQuery({
-    queryKey: [`/users/${userId}`],
+    queryKey: ['user', userId],
     queryFn: () => get<User>(`/users/${userId}`),
     retry: (failureCount: number, error: Error) =>
       error instanceof ApiError && error.status ? false : failureCount < 2,
   });
 
-  useEffect(() => {
-    if (data) dispatch(userAdded(data));
-  }, [data, dispatch]);
-
   if (isError) {
     console.error(error);
   }
 
-  return user;
+  return data;
 };
 
 export default useUserFetcher;
