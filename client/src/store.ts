a56c2de4c2ce5f5e34434130d0225154cf6472c8
Mebrahtom Kidane <mebrahtomkg1@gmail.com>
2025-09-16 09:21:53 +0300

    cleanup: remove chats slice as it is replaced by ReactQuery

--- client/src/store.ts
+++ client/src/store.ts
@@ -2,7 +2,6 @@ import { configureStore, createSelector } from '@reduxjs/toolkit';
 import profilePhotosReducer from '@/features/Settings/slices/profilePhotosSlice';
 import accountReducer from '@/features/Settings/slices/accountSlice';
 import usersReducer from '@/usersSlice';
-import chatsReducer from '@/features/Chat/slices/chatsSlice';
 import messageRequestsReducer from '@/features/Chat/slices/messageRequestsSlice';
 import contactsReducer from '@/contactsSlice';
 import blockedUsersReducer from '@/blockedUsersSlice';
@@ -12,8 +11,6 @@ const store = configureStore({
     account: accountReducer,
 
     profilePhotos: profilePhotosReducer,
-
-    chats: chatsReducer,
 
     messageRequests: messageRequestsReducer,
 
