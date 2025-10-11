e439c7c6fac72b8a63e2d302a0971f0a6ab2b7d7
Mebrahtom Kidane <mebrahtomkg1@gmail.com>
2025-10-11 21:49:51 +0300

    feat: improve Account update processor for handling edge cases and error handling

--- client/src/App/AccountUpdateProcessor.tsx
+++ client/src/App/AccountUpdateProcessor.tsx
@@ -1,6 +1,6 @@
 import { ApiError, put } from '@/api';
 import { QUERY_KEY_ACCOUNT } from '@/constants';
-import { useAbortController, useIsChanged } from '@/hooks';
+import { useAbortController, useStableValue } from '@/hooks';
 import queryClient from '@/queryClient';
 import useAccountUpdateRequestStore, {
   AccountUpdateRequest,
@@ -8,25 +8,19 @@ import useAccountUpdateRequestStore, {
 } from '@/store/useAccountUpdateRequestStore';
 import { Account } from '@/types';
 import { useMutation } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
 
 const AccountUpdateProcessor = () => {
   const { prepareAbortController, getSignal, abort } = useAbortController();
-  const request = useAccountUpdateRequestStore();
-  const isRequestChanged = useIsChanged(request);
+  const request = useStableValue(useAccountUpdateRequestStore());
+  const inServerAccountRef = useRef<Account>(
+    queryClient.getQueryData([QUERY_KEY_ACCOUNT]) as Account, // Loggedin user always have account
+  );
 
-  const { mutate } = useMutation({
+  const { mutate, variables } = useMutation({
     mutationFn: (req: AccountUpdateRequest) => {
       prepareAbortController(); // This will abort previous request and create new controller.
       return put<Account>('/users/me', req.payload, { signal: getSignal() });
-    },
-    onSuccess: (data, req, _context) => {
-      // If there is no request in the store, or if the processed request is the
-      // request in the store delete the request from store and update query data.
-      if (!request || req.id === request?.id) {
-        deleteAccountUpdateRequest();
-        queryClient.setQueryData([QUERY_KEY_ACCOUNT], () => data);
-        console.info('[****]Account Updated');
-      }
     },
     retry: (_failureCount: number, error: Error) => {
       // No retry if the request was aborted
@@ -37,25 +31,56 @@ const AccountUpdateProcessor = () => {
       }
       return true;
     },
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey: [QUERY_KEY_ACCOUNT] });
+    },
+    onError(error, _req) {
+      if (error instanceof ApiError && error.status) {
+        // Delete any existing request. even if it is new request as it may hold the error
+        // causing account update data due to deduplication made by store
+        deleteAccountUpdateRequest();
+
+        // Rolling back to in server pure account! because previous query data
+        // can be stale and incorrect due to the deduplication and merging made by the store
+        queryClient.setQueryData<Account>(
+          [QUERY_KEY_ACCOUNT],
+          inServerAccountRef.current,
+        );
+      }
+    },
+    onSuccess: (data, req) => {
+      inServerAccountRef.current = data;
+      const inStoreRequest = useAccountUpdateRequestStore.getState();
+      queryClient.setQueryData<Account>([QUERY_KEY_ACCOUNT], () => {
+        // If there is a new request in store, do merge for optimistic update.
+        if (inStoreRequest && inStoreRequest.id !== req.id) {
+          return { ...data, ...inStoreRequest.payload };
+        }
+        return data;
+      });
+      // If the curently processed request is in store, delete it
+      if (inStoreRequest && inStoreRequest.id === req.id) {
+        deleteAccountUpdateRequest();
+      }
+      console.info('[****]Account Updated');
+    },
   });
 
-  const doUpdate = async () => {
-    if (!request) return;
+  useEffect(() => {
+    if (request) {
+      queryClient.setQueryData([QUERY_KEY_ACCOUNT], (oldAccount) => {
+        if (!oldAccount) return undefined;
+        return { ...oldAccount, ...request.payload };
+      });
+    }
+  }, [request]);
 
-    // Immediately update query data instead of waiting mutation life cycles
-    queryClient.setQueryData([QUERY_KEY_ACCOUNT], (oldAccount) => {
-      if (!oldAccount) return undefined; // doesn't happen in reality
-      return { ...oldAccount, ...request.payload };
-    });
-
-    // Abort current ongoing requests
-    abort();
-    await queryClient.cancelQueries({ queryKey: [QUERY_KEY_ACCOUNT] });
-
-    mutate(request);
-  };
-
-  if (isRequestChanged && request) doUpdate();
+  useEffect(() => {
+    if (request && variables?.id !== request.id) {
+      abort();
+      mutate(request);
+    }
+  }, [request, variables?.id, abort, mutate]);
 
   return null;
 };
