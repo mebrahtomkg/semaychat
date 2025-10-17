ae09cb6db39f468aa07839f690e56bf6f685fa51
Mebrahtom Kidane <mebrahtomkg1@gmail.com>
2025-10-17 16:52:34 +0300

    feat: run action of menu item after the menu modal is unmounted

--- client/src/features/Home/components/Hamburger/index.tsx
+++ client/src/features/Home/components/Hamburger/index.tsx
@@ -1,4 +1,4 @@
-import { useAccountInfo, useImageLoader } from '@/hooks';
+import { useAccountInfo, useImageLoader, useLogout } from '@/hooks';
 import {
   HamburgerContainer,
   HamburgerStyled,
@@ -7,9 +7,20 @@ import {
   ProfilePhoto,
   ProfilePhotoContainer,
 } from './styles';
-import { useCallback, useState } from 'react';
-import Menu from '../Menu';
+import { useCallback, useMemo, useRef, useState } from 'react';
+import Menu, { MenuItemProps } from '../Menu';
 import { ANIMATION_DIALOG_FAST, WithAnimation } from '@/Animation';
+import { toggleTheme, useAppStateStore, useThemeStore } from '@/store';
+import {
+  ContactsIcon,
+  LogoutIcon,
+  MoonIcon,
+  ProfileIcon,
+  SettingsIcon,
+  SunIcon,
+} from '@/components/icons';
+
+type Action = 'openProfile' | 'openSettings' | 'openContacts';
 
 const Hamburger = () => {
   const { fullName, nameInitials, photoUrl } = useAccountInfo();
@@ -17,9 +28,66 @@ const Hamburger = () => {
   const { imageSrc, handleImageLoad, handleImageLoadError } =
     useImageLoader(photoUrl);
 
+  const action = useRef<Action | null>(null);
+
   const [isMenuVisible, setIsMenuVisible] = useState(false);
   const openMenu = useCallback(() => setIsMenuVisible(true), []);
   const closeMenu = useCallback(() => setIsMenuVisible(false), []);
+
+  const openContactsModal = useAppStateStore((s) => s.openContactsModal);
+  const openSettingsModal = useAppStateStore((s) => s.openSettingsModal);
+  const openProfileModal = useAppStateStore((s) => s.openProfileModal);
+  const theme = useThemeStore();
+
+  const logout = useLogout();
+
+  const openProfile = useCallback(() => {
+    action.current = 'openProfile';
+    closeMenu();
+  }, [closeMenu]);
+
+  const openSettings = useCallback(() => {
+    action.current = 'openSettings';
+    closeMenu();
+  }, [closeMenu]);
+
+  const openContacts = useCallback(() => {
+    action.current = 'openContacts';
+    closeMenu();
+  }, [closeMenu]);
+
+  const runAction = useCallback(() => {
+    const actionValue = action.current;
+    action.current = null;
+    switch (actionValue) {
+      case 'openProfile':
+        openProfileModal();
+        break;
+
+      case 'openSettings':
+        openSettingsModal();
+        break;
+
+      case 'openContacts':
+        openContactsModal();
+        break;
+    }
+  }, [openProfileModal, openSettingsModal, openContactsModal]);
+
+  const menuItems: MenuItemProps[] = useMemo(
+    () => [
+      { onClick: openContacts, icon: <ContactsIcon />, label: 'Contacts' },
+      { onClick: openProfile, icon: <ProfileIcon />, label: 'My Profile' },
+      { onClick: openSettings, icon: <SettingsIcon />, label: 'Settings' },
+      { onClick: logout, icon: <LogoutIcon />, label: 'Log out' },
+      {
+        onClick: toggleTheme,
+        icon: theme === 'light' ? <MoonIcon /> : <SunIcon />,
+        label: 'Theme',
+      },
+    ],
+    [openContacts, openProfile, openSettings, logout, theme],
+  );
 
   return (
     <>
@@ -44,7 +112,14 @@ const Hamburger = () => {
       <WithAnimation
         isVisible={isMenuVisible}
         options={ANIMATION_DIALOG_FAST}
-        render={(style) => <Menu onClose={closeMenu} animationStyle={style} />}
+        render={(style) => (
+          <Menu
+            menuItems={menuItems}
+            onClose={closeMenu}
+            animationStyle={style}
+          />
+        )}
+        onUnmount={runAction}
       />
     </>
   );
--- client/src/features/Home/components/Menu/index.tsx
+++ client/src/features/Home/components/Menu/index.tsx
@@ -1,5 +1,4 @@
-import React, { CSSProperties, FC } from 'react';
-import { useLogout } from '@/hooks';
+import React, { CSSProperties, FC, ReactNode, useMemo } from 'react';
 import {
   IconContainer,
   MenuItemButton,
@@ -7,93 +6,38 @@ import {
   MenuOverlay,
   MenuStyled,
 } from './styles';
-import {
-  ContactsIcon,
-  LogoutIcon,
-  MoonIcon,
-  ProfileIcon,
-  SettingsIcon,
-  SunIcon,
-} from '@/components/icons';
-import { toggleTheme, useAppStateStore, useThemeStore } from '@/store';
+
+export interface MenuItemProps {
+  icon: ReactNode;
+  label: string;
+  onClick: () => void;
+}
 
 interface MenuProps {
+  menuItems: MenuItemProps[];
   onClose: () => void;
   animationStyle?: CSSProperties;
 }
 
-const Menu: FC<MenuProps> = ({ onClose, animationStyle }) => {
-  const openContactsModal = useAppStateStore(
-    (state) => state.openContactsModal,
-  );
-
-  const openSettingsModal = useAppStateStore(
-    (state) => state.openSettingsModal,
-  );
-
-  const openProfileModal = useAppStateStore((state) => state.openProfileModal);
-
-  const logout = useLogout();
-
-  const openProfile = () => {
-    onClose();
-    openProfileModal();
-  };
-
-  const openSettings = () => {
-    onClose();
-    openSettingsModal();
-  };
-
-  const openContacts = () => {
-    onClose();
-    openContactsModal();
-  };
-
-  const theme = useThemeStore();
-
-  const menuItems = [
-    {
-      onClick: openContacts,
-      icon: <ContactsIcon />,
-      label: 'Contacts',
-    },
-    {
-      onClick: openProfile,
-      icon: <ProfileIcon />,
-      label: 'My Profile',
-    },
-    {
-      onClick: openSettings,
-      icon: <SettingsIcon />,
-      label: 'Settings',
-    },
-    {
-      onClick: logout,
-      icon: <LogoutIcon />,
-      label: 'Log out',
-    },
-    {
-      onClick: toggleTheme,
-      icon: theme === 'light' ? <MoonIcon /> : <SunIcon />,
-      label: 'Theme',
-    },
-  ];
-
+const Menu: FC<MenuProps> = ({ menuItems, onClose, animationStyle }) => {
   const handleOverlayClick = (e: React.MouseEvent<HTMLElement>) => {
     if (e.target === e.currentTarget) onClose();
   };
 
+  const menuItemComponents = useMemo(
+    () =>
+      menuItems.map((item) => (
+        <MenuItemButton key={item.label} onClick={item.onClick}>
+          <IconContainer>{item.icon}</IconContainer>
+          <MenuItemLabel>{item.label}</MenuItemLabel>
+        </MenuItemButton>
+      )),
+    [menuItems],
+  );
+
   return (
     <MenuOverlay onClick={handleOverlayClick}>
-      <MenuStyled style={animationStyle}>
-        {menuItems.map((item) => (
-          <MenuItemButton key={item.label} onClick={item.onClick}>
-            <IconContainer>{item.icon}</IconContainer>
-            <MenuItemLabel>{item.label}</MenuItemLabel>
-          </MenuItemButton>
-        ))}
-      </MenuStyled>
+      <MenuStyled style={animationStyle}>{menuItemComponents}</MenuStyled>
     </MenuOverlay>
   );
 };
