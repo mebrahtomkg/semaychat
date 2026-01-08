326d981e7f31b0e1fba6e7915ffb7d901af65fb7
Mebrahtom Kidane <mebrahtomkg1@gmail.com>
2026-01-08 16:53:56 +0300

    feat: improve signup form validation schema

--- client/src/features/Guest/components/SignUpForm/schema.ts
+++ client/src/features/Guest/components/SignUpForm/schema.ts
@@ -3,7 +3,10 @@ import * as z from 'zod';
 export const firstSignUpFormSchema = z.object({
   firstName: z.string().min(1, 'First name is required'),
   lastName: z.string().optional(),
-  email: z.string().email('Invalid email address').min(1, 'Email is required'),
+  email: z.email({
+    error: (iss) =>
+      !iss.input ? 'Email address is required' : 'Invalid email address',
+  }),
 });
 export type FirstSignUpFormSchema = z.infer<typeof firstSignUpFormSchema>;
 
@@ -11,7 +14,12 @@ export const secondSignUpFormSchema = z
   .object({
     password: z
       .string()
-      .min(6, 'Password must be at least 6 characters')
+      .min(6, {
+        error: (iss) =>
+          !iss.input
+            ? 'Password is required'
+            : 'Password must be at least 6 characters',
+      })
       .max(50, 'Password is too long'),
     confirmPassword: z.string().min(1, 'Confirm Password is required'),
   })
